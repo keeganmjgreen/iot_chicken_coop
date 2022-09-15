237477a4f0813669074cfab17a6f738baaa0322d
Keegan Green <keegan.green@bluwave-ai.com>
2022-09-15 18:30:22 -0400

    motorized_gate: added feedback with a motor encoder
    encoder that gets calibrated by the hard stop limit switches and provides feedback even when program waits for motor to spin down
    
    also send multiple stop commands/pulses to esc to make sure it receives and stops

--- motorized_gate/motorized_gate.ino
+++ motorized_gate/motorized_gate.ino
@@ -2,7 +2,7 @@
 #define MIN_PULSE 1000 // microseconds
 #define MAX_PULSE 2000 // microseconds
 #define ARM_PULSE  500 // microseconds
-#define RUNDOWN_TIME_MS 0.5e3
+#define RUNDOWN_TIME_MS 2000
 
 #include "ESC.h"
 ESC my_esc(ESC_PIN, MIN_PULSE, MAX_PULSE, ARM_PULSE);
@@ -13,18 +13,47 @@ Multi_Channel_Relay relays;
 float currently_set_speed = 0;
 // Note: The *currently set* speed is not necessarily the *current* speed.
 
-void set_speed(float newly_set_speed);
+bool still_setting_speed = false;
+unsigned long rundown_start_time_ms = 0;
+float newly_set_speed;
+const float LO_SPEED = 0.6;
+const float HI_SPEED = 1.0;
+void set_speed(float _newly_set_speed);
+void continue_setting_speed();
 
 #define close_button_PIN 23
-#define open_button_PIN 33
+#define  open_button_PIN 33
+
 bool close_button_old_state;
+bool  open_button_old_state;
+
 bool close_button_new_state;
-bool open_button_old_state;
-bool open_button_new_state;
+bool  open_button_new_state;
+
 bool close_button_rising_edge;
+bool  open_button_rising_edge;
+
 bool close_button_falling_edge;
-bool open_button_rising_edge;
-bool open_button_falling_edge;
+bool  open_button_falling_edge;
+
+#define ENC_PIN_A 22 // Pin for Encoder Channel A.
+#define ENC_PIN_B 19 // Pin for Encoder Channel B.
+
+bool enc_calibrated = false;
+const float ANGLE_THRES_DEG = 5;
+
+bool old_enc_state_A; // Old Encoder Channel A signal.
+bool old_enc_state_B; // Old Encoder Channel B signal.
+
+bool new_enc_state_A; // New Encoder Channel A signal.
+bool new_enc_state_B; // New Encoder Channel B signal.
+
+const float ANGLE_MIN_DEG =  0;
+const float ANGLE_MAX_DEG = 79;
+
+float angle_deg; // Current door angle (relative - incremental encoder) (0.5-degree resolution).
+
+float start_angle_deg;
 
 void setup()
 {
@@ -35,11 +64,14 @@ void setup()
     my_esc.arm();
 
     pinMode(close_button_PIN, INPUT_PULLUP);
-    pinMode(open_button_PIN, INPUT_PULLUP);
-    close_button_new_state = !digitalRead(close_button_PIN);
-    open_button_new_state = !digitalRead(open_button_PIN);
-}
+    pinMode( open_button_PIN, INPUT_PULLUP);
 
+    close_button_new_state = !digitalRead(close_button_PIN);
+     open_button_new_state = !digitalRead( open_button_PIN);
+
+    pinMode(ENC_PIN_A, INPUT_PULLUP);
+    pinMode(ENC_PIN_B, INPUT_PULLUP);
+}
 void loop()
 {
     if (Serial.available() > 0)
@@ -48,57 +80,167 @@ void loop()
     }
 
     close_button_old_state = close_button_new_state;
-    close_button_new_state = !digitalRead(close_button_PIN);
-    open_button_old_state = open_button_new_state;
-    open_button_new_state = !digitalRead(open_button_PIN);
-    close_button_rising_edge = !close_button_old_state & close_button_new_state;
-    close_button_falling_edge = close_button_old_state & !close_button_new_state;
-    open_button_rising_edge = !open_button_old_state & open_button_new_state;
-    open_button_falling_edge = open_button_old_state & !open_button_new_state;
+     open_button_old_state =  open_button_new_state;
 
-    if (((currently_set_speed > 0) & (close_button_rising_edge | open_button_falling_edge)) |
-        ((currently_set_speed < 0) & (open_button_rising_edge | close_button_falling_edge)))
+    close_button_new_state = !digitalRead(close_button_PIN);
+     open_button_new_state = !digitalRead( open_button_PIN);
+
+    close_button_rising_edge = !close_button_old_state & close_button_new_state;
+     open_button_rising_edge = ! open_button_old_state &  open_button_new_state;
+
+    close_button_falling_edge = close_button_old_state & !close_button_new_state;
+     open_button_falling_edge =  open_button_old_state & ! open_button_new_state;
+
+    if ((currently_set_speed > 0) & (close_button_rising_edge | open_button_falling_edge))
     {
+        if (close_button_rising_edge)
+        {
+            if (!enc_calibrated | (enc_calibrated & (ANGLE_MAX_DEG - angle_deg < ANGLE_THRES_DEG)))
+            {
+                new_enc_state_A = digitalRead(ENC_PIN_A);
+                new_enc_state_B = digitalRead(ENC_PIN_B);
+                angle_deg = ANGLE_MAX_DEG;
+                enc_calibrated = true;
+            }
+        }
+        set_speed(0);
+    }
+    else if ((currently_set_speed < 0) & (open_button_rising_edge | close_button_falling_edge))
+    {
+        if (open_button_rising_edge)
+        {
+            if (!enc_calibrated | (enc_calibrated & (angle_deg - ANGLE_MIN_DEG < ANGLE_THRES_DEG)))
+            {
+                new_enc_state_A = digitalRead(ENC_PIN_A);
+                new_enc_state_B = digitalRead(ENC_PIN_B);
+                angle_deg = ANGLE_MIN_DEG;
+                enc_calibrated = true;
+            }
+        }
         set_speed(0);
     }
     else if (currently_set_speed == 0)
     {
         if (!open_button_new_state & close_button_falling_edge)
         {
-            set_speed(-0.4);
+            set_speed(-LO_SPEED);
+            start_angle_deg = angle_deg;
         }
         else if (!close_button_new_state & open_button_falling_edge)
         {
-            set_speed(+0.4);
+            set_speed(+LO_SPEED);
+            start_angle_deg = angle_deg;
         }
     }
-    delay(100);
+    if (still_setting_speed & ((millis() - rundown_start_time_ms) > RUNDOWN_TIME_MS))
+    {
+        continue_setting_speed();
+        still_setting_speed = false;
+    }
+    if (enc_calibrated)
+    {
+        old_enc_state_A = new_enc_state_A;
+        old_enc_state_B = new_enc_state_B;
+    
+        new_enc_state_A = digitalRead(ENC_PIN_A);
+        new_enc_state_B = digitalRead(ENC_PIN_B);
+    
+        if ((!old_enc_state_A && new_enc_state_B || old_enc_state_A && !new_enc_state_B) &&
+            (!old_enc_state_B && !new_enc_state_A || old_enc_state_B && new_enc_state_A))
+        {
+            angle_deg += 0.5;
+        }
+        if ((old_enc_state_A && new_enc_state_B || !old_enc_state_A && !new_enc_state_B) &&
+            (!old_enc_state_B && new_enc_state_A || old_enc_state_B && !new_enc_state_A))
+        {
+            angle_deg -= 0.5;
+        }
+             if (angle_deg < ANGLE_MIN_DEG) {angle_deg = ANGLE_MIN_DEG;}
+        else if (angle_deg > ANGLE_MAX_DEG) {angle_deg = ANGLE_MAX_DEG;}
+
+        if (Serial.availableForWrite() >= 6)
+        {
+            Serial.println(angle_deg);
+        }
+        if (newly_set_speed > 0)
+        {
+            // set_speed(+0.5 + 0.6 * (ANGLE_MAX_DEG - angle_deg) / (ANGLE_MAX_DEG - start_angle_deg));
+            if (angle_deg > ANGLE_MAX_DEG - 15)
+            {
+                set_speed(+LO_SPEED);
+            }
+            else
+            {
+                set_speed(+HI_SPEED);
+            }
+        }
+        else if (newly_set_speed < 0)
+        {
+            // set_speed(-0.5 - 0.6 * (angle_deg - ANGLE_MIN_DEG) / (start_angle_deg - ANGLE_MIN_DEG));
+            if (angle_deg < ANGLE_MIN_DEG + 15)
+            {
+                set_speed(-LO_SPEED);
+            }
+            else
+            {
+                set_speed(-HI_SPEED);
+            }
+        }
+    }
+    delay(10);
 }
 
-void set_speed(float newly_set_speed)
+void set_speed(float _newly_set_speed)
 {
-    // Change the speed of the motor, knowing its currently set speed...
+    if (!still_setting_speed)
+    {
+        // Change the speed of the motor, knowing its currently set speed...
+    
+        // The unsigned speed of the motor is controlled by the electronic speed
+        // controller (ESC), while its direction is controlled by the relay module.
 
-    // The unsigned speed of the motor is controlled by the electronic speed
-    // controller (ESC), while its direction is controlled by the relay module.
+        newly_set_speed = _newly_set_speed;
 
+        if (currently_set_speed * newly_set_speed <= 0)
+        {
+            // If the currently and newly set speeds have opposite signs,
+            // or if either are zero...
+    
+            // First, set the speed to zero using the ESC
+            // (if the currently set speed is not already zero):
+            my_esc.speed(MIN_PULSE);
+            delay(10);
+            my_esc.speed(MIN_PULSE);
+            delay(10);
+            my_esc.speed(MIN_PULSE);
+            delay(10);
+            my_esc.speed(MIN_PULSE);
+            delay(10);
+            my_esc.speed(MIN_PULSE);
+    
+            // Next, wait for the motor to physically stop:
+            if (currently_set_speed != 0)
+            {
+                rundown_start_time_ms = millis();
+                still_setting_speed = true;
+            }
+            // It is assumed that no channels of the relay module
+            // should be turned off while the motor is running.
+            else
+            {
+                continue_setting_speed();
+            }
+        }
+        else
+        {
+            continue_setting_speed();
+        }
+    }
+}
+void continue_setting_speed()
+{
     if (currently_set_speed * newly_set_speed <= 0)
     {
-        // If the currently and newly set speeds have opposite signs,
-        // or if either are zero...
-
-        // First, set the speed to zero using the ESC
-        // (if the currently set speed is not already zero):
-        my_esc.speed(MIN_PULSE);
-
-        // Next, wait for the motor to physically stop:
-        if (currently_set_speed != 0)
-        {
-            delay(RUNDOWN_TIME_MS);
-        }
-        // It is assumed that no channels of the relay module
-        // should be turned off while the motor is running.
-
         if (newly_set_speed > 0)
         {
             // If the newly set speed is positive and nonzero,
